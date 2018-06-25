54e2104166ec1565fdea68d3db0e7661de7301d0
TravisPace <40004711+TravisPace@users.noreply.github.com>
2018-06-25 00:11:46 -0500

    Create Quiz03.py

---
+++ Quiz03.py
@@ -0,0 +1,95 @@
+#1
+def dot(vector01,vector02):
+  '''
+  This function calculates the dot product of the two vectors.
+  '''
+  C = [ ]
+  #C is an empty list.
+  if len(vector01) != len(vector02):
+    print('error')
+    return None
+  #If the lengths of the vectors are not equal to each other, then the algorithim does not compute and returns none.
+  for i in range(len(vector01)):
+   #For the length of the vector i.
+    C.append(vector01[i] * vector02[i])
+    #multiplies each element together and creates a list C.
+  D = 0
+  #A new integer D is made.
+  for i in range(len(C)):
+    D = D + C[i]
+  return D
+  #The function then adds the elements in C toghether to get a single integer, D.
+  
+vector01 = [1,2]
+vector02 = [3,4]
+print(dot(vector01,vector02))
+
+#2
+def vecSubtraction(vector01,vector02):
+  '''
+  This function adds to vectors together.
+  '''
+  A = [ ]
+  #A is an empty list.
+  if len(vector01) != len(vector02):
+    print('error')
+    return None
+  #If the lengths of the lists vector01 and vector02 do not equal each other, then this prints an error and returns none.
+  for i in range(len(vector1)):
+    A.append(vector01[i] - vector02[i])
+    #This asubtracts the lengths of vector01 and vector02 and adds each element of the list together.
+  return A
+  #This returns the list A.
+
+V1 = [1,2]
+V2 = [3,4]
+print(vecAddition(V1,V2))
+
+#3
+def scalarMultVec(scalar,vector):
+  '''
+  This function takes a scalar and a vector and multiplies them together.
+  '''
+  if type(scalar) == list:
+    print('error')
+    return None
+  #If the scalar is a list, then the algorithim will print error and return none.
+  A = [ ]
+  #A is an empty list.
+  for i in range(len(vector)):
+    #For the length of the vector.
+    A.append(vector[i] * scalar)
+    #For each element in length i, multiply by the scalar.
+  return A
+#This creates a new list for A and returns A.
+
+#4
+def infNorm(vector):
+  '''
+  This takes the infinity norm of the vector.
+  '''
+  result = 0
+  #Result is equal to no integer.
+  for i in range(len(vector)):
+  #For every element in the vector.
+    if result <= abs(vector[i]):
+    #This takes each element of the vector and sees if it greater than or equal to the previous element in the vector list.
+      result = abs(vector[i])
+      #If the result is greater than or equal to than the previous element, then the element becomes the result.
+  return result
+#This returns the integer as the result.
+
+#5
+def normalize(vector):
+  '''
+  This function normalizes the vector
+  '''
+  result = infNorm(vector)
+  if result == 1 or result == 0:
+    return vector
+  #If the result is equal to 1 or zero from the infinity norm function, then this will return the vector.
+  scalar = 1/result
+  B = scalarMultVec(scalar,vector)
+  #This takes the scalar multiplication vector function and runs it to get B, B is the vector and scalar is scalar.
+  return B
+#This returns the vector B.
